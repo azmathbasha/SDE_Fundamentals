df968bdf9b038aba51c091080c49f9895401d4fb
Azmath Bahsa <azmathbasha.r@gmail.com>
2024-08-02 18:24:10 +0000

    Adding simple programs

---
+++ SimplePrograms/AddNumbers.java
@@ -0,0 +1,6 @@
+public class AddNumbers {
+    public static void main(String[] args) {
+        int num = Integer.parseInt(args[0]) + Integer.parseInt(args[1]);
+        System.out.println("Addition = " + num);
+    }
+}
--- SimplePrograms/FirstProgram.java
+++ SimplePrograms/FirstProgram.java
@@ -3,4 +3,5 @@ class FirstProgram{
     public static void main(String []args){
         System.out.println("Nerd 2 Pro");
     }
+
 }
